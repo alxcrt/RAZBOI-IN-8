4540ba3a217126211889bc61a7ac244e6b918784
Iftimoaia Razvan <razvaniftimoaia20@yahoo.ro>
2020-12-04 12:45:20 +0200

    hatz gionutule

--- src/Game.cpp
+++ src/Game.cpp
@@ -11,47 +11,37 @@ void game() {
   while (1) {
     //drawButton(menuButton);
     // outtextxy(getmaxx() / 2, getmaxy() / 2 + 6, (char*)"Jocul este in constructie");
-    setcolor(RED);
-    setfillstyle(SOLID_FILL, RED);
-    rectangle(0, 0, 1200, 800);
-    floodfill(100, 100, RED);
     drawTable(getmaxx() / 2, getmaxy() / 2, 600, 600);
     if (menuButton.isPressed) {
       menu();
     }
 
-    delay(50);
+    delay(100);
   }
 }
 
 void drawTable(int x, int y, int width, int height) {
   int i, j, k;
+  setcolor(RED);
+  setfillstyle(SOLID_FILL, RED);
+  rectangle(0, 0, getmaxx(), getmaxy());
+  floodfill(100, 100, RED);
   k = width / 8;
-  setcolor(WHITE);
-  setlinestyle(0,1,1);
   for (i = 0; i < 8; i++)
     for (j = 0; j < 8; j++) {
-      //if (i % 2 == 0 && j % 2 == 0)
-      //setcolor(WHITE);
-      //else if (i % 2 == 1 && j % 2 == 1)
-      //setcolor(WHITE);
-      //else setcolor(BLACK);
-      rectangle(x - (300 - i * k), y - (300 - j * k), x - (300 - k - i * k), y - (300 - k - j * k));
-      if (i % 2 == 0 && j % 2 == 0) {
+      if ((i+j)%2==0) {
+        setcolor(WHITE);
+        rectangle(x - (width / 2 - i * k), y - (width / 2 - j * k), x - (width / 2 - k - i * k), y - (width / 2 - k - j * k));
         setfillstyle(SOLID_FILL, WHITE);
-        floodfill(x - (290 - i * k), y - (290 - j * k), WHITE);
-      } else if (i % 2 == 1 && j % 2 == 1) {
-        setfillstyle(SOLID_FILL, WHITE);
-        floodfill(x - (290 - i * k), y - (290 - j * k), WHITE);
-      } else {
-        {
-          setfillstyle(SOLID_FILL, BLACK);
-          floodfill(x - (290 - i * k), y - (290 - j * k), WHITE);
-        }
+        floodfill(x - ((width / 2 - 1) - i * k), y - ((width / 2 - 1) - j * k), WHITE);
       }
+        else 
+        {
+          setcolor(BLACK);
+          rectangle(x - (width / 2 - i * k)+1, y - (width / 2 - j * k)+1, x - (width / 2 - k - i * k)-1, y - (width / 2 - k - j * k)-1);
+          setfillstyle(SOLID_FILL, BLACK);
+          floodfill(x - ((width / 2 - 10) - i * k), y - ((width / 2 - 10) - j * k), BLACK);
+        }
+      
     }
 }
-
-//culoare pt background
-//tabla cu patratele ca la sah
-//
